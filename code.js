73ffea33efcf3162e250fe168813b7011d8a375b
Nataliee97 <glucha3@gmail.com>
2023-11-17 14:54:13 +0100

    wyświetlenie info z localStorage

--- code.js
+++ code.js
@@ -7,9 +7,17 @@ if (input) {
       console.log(input.value);
       var allInputs = JSON.parse(localStorage.getItem("allInputs")) || [];
       allInputs.push(input.value);
+      input.value = "";
       localStorage.setItem("allInputs", JSON.stringify(allInputs));
       document.getElementById("output").innerHTML =
         localStorage.getItem("allInputs");
     }
   });
+}
+function view() {
+  if (localStorage.getItem("data") != null) {
+    document.getElementById("output").innerHTML = JSON.parse(
+      localStorage.getItem("data")
+    );
+  }
 }
