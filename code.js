92efc366760e2a87a3f5504dfabad27c5078bbdb
Nataliee97 <glucha3@gmail.com>
2023-11-21 14:30:35 +0100

    output

--- code.js
+++ code.js
@@ -1,22 +1,18 @@
 var input = document.getElementById("input");
 var output = document.getElementById("output");
 var allInputs = JSON.parse(localStorage.getItem("allInputs")) || [];
-var setInputs = localStorage.setItem("allInputs", JSON.stringify(allInputs));
 
 if (input) {
   input.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
       event.preventDefault();
-      console.log(input.value);
       allInputs.push(input.value);
       input.value = "";
-      setInputs;
-      output.innerHTML = allInputs;
+      localStorage.setItem("allInputs", JSON.stringify(allInputs));
+      for (i = 0; i < allInputs.length; i++) {
+        console.log(allInputs[i]); // wyświetla wszystkie
+        output.innerHTML = allInputs[i]; // tylko ostatni
+      }
     }
   });
-}
-function view() {
-  if (allInputs != null) {
-    output.innerHTML = allInputs;
-  }
 }
