dad1044e7a6944f4dff589a6da48b078dbdfce05
Nataliee97 <glucha3@gmail.com>
2023-11-30 10:19:03 +0100

    added tasks to output

--- code.js
+++ code.js
@@ -1,25 +1,28 @@
 var input = document.getElementById("input");
-var output = document.getElementById("output");
+var output = document.querySelector("ul");
 var tasks = [];
 var setItems = function (x) {
   localStorage.setItem("tasks", JSON.stringify(x));
 };
 var getItems = function () {
+  // usuwanie li
   tasks = JSON.parse(localStorage.getItem("tasks"));
+  const list = tasks.map((el) => {
+    console.log(output);
+    var li1 = document.createElement("li");
+    li1.textContent = el;
+    output.appendChild(li1);
+  });
+  console.log(tasks);
 };
 
 input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    const task = {
-      id: new Date().getTime(),
-      name: input.value,
-    };
+    const task = input.value;
     tasks.push(task);
     input.value = "";
     setItems(tasks);
     getItems();
   }
 });
-
-// function renderTasks() {tasks.map(el => )}
